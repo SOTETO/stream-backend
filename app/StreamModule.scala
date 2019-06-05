b2cfa25dd3b7356510f965a1db343a0384288b17
Dennis Kleber <dennis_kleber@mailbox.org>
2019-06-05 15:08:49 +0200

    remove the InMemoryHouseHoldDAO adds evolution for Deposit

--- app/StreamModule.scala
+++ app/StreamModule.scala
@@ -1,12 +1,12 @@
 import daos._
 import play.api.{Configuration, Environment}
-import services.HouseholdService
+//import services.HouseholdService
 
 class StreamModule extends play.api.inject.Module {
   def bindings(environment: Environment, configuration: Configuration) = {
     Seq(
       bind[DepositDAO].to[MariaDBDepositDAO],
-      bind[HouseholdDAO].to[InMemoryHousholdDAO],
+      bind[HouseholdDAO].to[SQLHouseholdDAO],
       bind[DonationsDAO].to[SQLDonationsDAO],
       bind[UserDAO].to[DropsUserDAO]
     )
